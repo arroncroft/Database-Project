fc88a34e82c5a42995faca2ca99ff9a1f9389ac0
Arron Croft <arroncroft@hotmail.com>
2017-04-08 17:14:11 -0400

    query 1 and 3 done

--- db/Queries.java
+++ db/Queries.java
@@ -15,51 +15,92 @@ public class Queries {
 	private static Connection con;
 	private static Statement stmt;
 	private static ResultSet rs;
-
+   
+   public static void main(String[] args) throws Exception {
+         // opening database connection to MySQL server
+			con = DriverManager.getConnection(url, user, password);
+			System.out.println("Database connected successfully");
+         System.out.println("*******************************");
+         
+         query3(con,"action",10,1);//query test (top 10 action films)
+	}
 
 	private static void query1 (Connection conn, int topNum, int pgNum) throws SQLException {
-
-		String query = "SELECT m.rtAudienceRating, m.title, m.movieYear, m.rtPictureURL, m.imdbPictureURL "+ 
+      String query = "SELECT m.title, m.movieYear, m.rtAudienceRating, m.rtPictureURL, m.imdbPictureURL "+ 
                      "FROM movie m "+
                      "WHERE m.rtAudienceRating NOT LIKE '%N%' "+
                      "ORDER BY m.rtAudienceRating DESC "+
                      "LIMIT "+topNum+" OFFSET "+((pgNum-1)*topNum);
-
-		try {
-         //create the prepared statement and add the criteria
+      try {
+         //create the prepared statement
 			PreparedStatement ps = conn.prepareStatement(query);
-
-			//process the results
+         //process the results
 			ResultSet rs = ps.executeQuery();
-
-			while (rs.next()) {
-            System.out.println(rs.getString("rtAudienceRating"));
+         while (rs.next()) {
+            System.out.print(rs.getString("title")+" | "+
+                             rs.getInt("movieYear")+" | "+
+                             rs.getString("rtAudienceRating")+" | "+
+                             rs.getString("rtPictureURL")+" | "+
+                             rs.getString("imdbPictureURL")+"\n");
 			}
          rs.close();
 			ps.close();
-
 		}
-
-		catch (SQLException se) {
+      catch (SQLException se) {
 			se.printStackTrace();
 		}
-		}
-
-
-
-	public static void main(String[] args) throws Exception {
-         System.out.println("testing");
-         
-			// opening database connection to MySQL server
-			con = DriverManager.getConnection(url, user, password);
-			System.out.println("Database connected successfully");
-         
-         query1(con,10,1);
 	}
-
-
-
-
-
+   
+   /*private static void query2 (Connection conn, String title, int pgNum) throws SQLException {
+      String query = "SELECT m.title, m.movieYear, m.rtAudienceRating, m.rtPictureURL, m.imdbPictureURL "+ 
+                     "FROM movie m "+
+                     "WHERE m.rtAudienceRating NOT LIKE '%N%' "+
+                     "ORDER BY m.rtAudienceRating DESC "+
+                     "LIMIT "+topNum+" OFFSET "+((pgNum-1)*topNum);
+      try {
+         //create the prepared statement
+			PreparedStatement ps = conn.prepareStatement(query);
+         //process the results
+			ResultSet rs = ps.executeQuery();
+         while (rs.next()) {
+            System.out.print(rs.getString("title")+" | "+
+                             rs.getInt("movieYear")+" | "+
+                             rs.getString("rtAudienceRating")+" | "+
+                             rs.getString("rtPictureURL")+" | "+
+                             rs.getString("imdbPictureURL")+"\n");
+			}
+         rs.close();
+			ps.close();
+		}
+      catch (SQLException se) {
+			se.printStackTrace();
+		}
+	}*/
+   
+   private static void query3 (Connection conn, String genre, int topNum, int pgNum) throws SQLException {
+      String query = "SELECT m.title, m.movieYear, m.rtAudienceRating, m.rtPictureURL, m.imdbPictureURL "+ 
+                     "FROM movie m, movie_genres mg "+
+                     "WHERE m.movieID = mg.movieID and mg.genre = '"+genre.toUpperCase()+"' AND m.rtAudienceRating NOT LIKE '%N%' "+
+                     "ORDER BY m.rtAudienceRating DESC "+
+                     "LIMIT "+topNum+" OFFSET "+((pgNum-1)*topNum);
+      try {
+         //create the prepared statement
+			PreparedStatement ps = conn.prepareStatement(query);
+         //process the results
+			ResultSet rs = ps.executeQuery();
+         while (rs.next()) {
+            System.out.print(rs.getString("title")+" | "+
+                             rs.getInt("movieYear")+" | "+
+                             rs.getString("rtAudienceRating")+" | "+
+                             rs.getString("rtPictureURL")+" | "+
+                             rs.getString("imdbPictureURL")+"\n");
+			}
+         rs.close();
+			ps.close();
+		}
+      catch (SQLException se) {
+			se.printStackTrace();
+		}
+	}
 
 }
